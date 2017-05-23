886d392166d759a928a4e5281cbeec17ed6cb4dd
AllenChangCN <zhangkailongabc@163.com>
2017-05-23 22:41:36 +0800

    TEST

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
 /**
  * Created by zkl on 2017/5/9.
  */
+function check() {
+    var userName = document.getElementsByName("username")[0].value;
+    var passWd = document.getElementsByName("password")[0].value;
+    if(userName==""||passWd=="")
+    {
+        alert("Please input User Name!");
+    }
+}
